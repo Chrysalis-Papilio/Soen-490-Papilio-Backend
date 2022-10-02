445f7510c952bef5b52999b1c6814555234d10dc
Thanh Ta <mth.1012.ta@gmail.com>
2022-10-02 17:14:25 -0400

    Added some model files

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,8 @@ import express from "express";
 import bodyParser from "body-parser";
 
 const app = express();
-const userRouter = require("./route/user");
-const activityRouter = require("./route/activity");
+const userRouter = require("./routes/user-routes");
+const activityRouter = require("./routes/activity-routes");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -14,3 +14,5 @@ app.use("/activity", activityRouter);
 app.listen(3000, () => {
     console.log("Server listening on port 3000...");
 })
+
+module.exports = app;
