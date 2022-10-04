395408cff6c0382fcd2cacfbdc0db2ed97870a5d
Thanh Ta <mth.1012.ta@gmail.com>
2022-10-03 21:25:31 -0400

    PAPILIO-46 Created ActivityReview model

--- src/models/ActivityReview.ts
+++ src/models/ActivityReview.ts
@@ -1,3 +1,33 @@
-class ActivityReview {
+import {DataTypes, Model} from "sequelize";
+const Activity = require("./Activity");
+const User = require("./User");
+const database = require("../database");
 
+class ActivityReview extends Model {
+    declare id: number;
 }
+
+ActivityReview.init({
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    comment: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    rating: {
+        type: DataTypes.INTEGER,
+        validate: {
+            min: 1,
+            max: 10
+        },
+        allowNull: false
+    }
+}, {
+    sequelize: database
+})
+
+ActivityReview.belongsTo(Activity);
+ActivityReview.belongsTo(User);
