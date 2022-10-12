d7c949ddb243b1e62074b32f0745e58b16c84d8e
Thanh Ta <mth.1012.ta@gmail.com>
2022-10-12 13:59:32 -0400

    PAPILIO-48 Removed SQL logging

--- src/api/repos/user-repo.ts
+++ src/api/repos/user-repo.ts
@@ -1,54 +1,50 @@
-import { User } from '../models/User';
-import { httpStatusCode } from '../../types/httpStatusCodes';
-import { logger } from '../../config/logger';
-import { APIError } from '../../errors/api-error';
-import { BaseError } from '../../errors/base-error';
+import {User} from '../models/User';
+import {httpStatusCode} from '../../types/httpStatusCodes';
+import {logger} from '../../config/logger';
+import {APIError} from '../../errors/api-error';
+import {BaseError} from '../../errors/base-error';
 
 //** Get all accounts from table account */
 const getAllUsers = async () => {
-    await User.sync({ logging: false });
+    await User.sync();
     return User.findAll({ logging: false });
 };
 
 // Create a sample user with hardcoded info (test-only)
 const createSampleUser = async () => {
-    await User.sync({ alter: true, logging: false });
+    await User.sync();
     return await User.create({
         firstName: 'Sample',
         lastName: 'User',
         phone: '5145551237',
         email: 'sample4@gmail.com',
-        firebase_id: 'totallynotanid',
-        logging: false,
+        firebase_id: 'totallynotanid'
     });
 };
 
 //  Create a simple user with verified input
 const createSimpleUser = async (user: any) => {
-    await User.sync({ alter: true, logging: false });
-    const result = await User.create({
+    await User.sync();
+    return await User.create({
         firstName: user.firstName,
         lastName: user.lastName,
         email: user.email,
         countryCode: user.countryCode ? user.countryCode : undefined,
         phone: user.phone ? user.phone : undefined,
-        firebase_id: user.firebase_id,
-        logging: false
+        firebase_id: user.firebase_id
     }).catch((error) => {
         logger.error(error.message);
         if (error.message === 'Validation error')
             throw new APIError('This user already exists.', 'createSimpleUser', httpStatusCode.CONFLICT, true);
         throw new BaseError('ORM Sequelize Error.', 'There has been an error in the DB.', 'createSimpleUser', httpStatusCode.INTERNAL_SERVER, true);
     });
-    return result;
 };
 
 //  Get User from email
 const getUserByEmail = async (email: string) => {
-    await User.sync({ alter: true, logging: false });
+    await User.sync();
     const result = await User.findOne({
         where: { email: email },
-        logging: false
     });
     logger.info(`Getting user ${email}: ${result}.`);
     return result;
@@ -57,10 +53,9 @@ const getUserByEmail = async (email: string) => {
 // Update User
 // matcher: {email: 'email@here.com'} or {firstName: 'John', lastName: 'Doe'} or ...
 const updateUser = async (matcher: any, user: any) => {
-    await User.sync({ alter: true, logging: false });
+    await User.sync();
     return await User.update(user, {
         where: matcher,
-        logging: false
     });
 };
 
