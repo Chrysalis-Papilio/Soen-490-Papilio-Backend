c1fd40685c0f9ca1fecb5cf111f8cc9f93bc3e9b
Thanh Ta <mth.1012.ta@gmail.com>
2022-11-19 15:05:16 -0500

    Updated schema

--- src/api/schemas/business-schema.ts
+++ src/api/schemas/business-schema.ts
@@ -1,4 +1,4 @@
-import { object, string } from 'zod';
+import { any, number, object, string } from 'zod';
 import { activitySchema } from './activity-schema';
 import { addressSchema } from './address-schema';
 import { employeeSchema } from './employee-schema';
@@ -12,11 +12,35 @@ const getBusinessById = object({
     }).strict('Request URL contains an invalid key')
 });
 
+const getEmployee = object({
+    params: object({
+        businessId: string({
+            required_error: 'Business ID is required',
+            invalid_type_error: 'Business ID should be of type string'
+        }),
+        employeeId: string({
+            required_error: 'Employee Firebase ID is required',
+            invalid_type_error: 'Employee Firebase ID should be of type string'
+        })
+    }).strict('Request URL contains an invalid key')
+});
+
+const getActivity = object({
+    params: object({
+        businessId: string({
+            required_error: 'Business ID is required',
+            invalid_type_error: 'Business ID should be of type string'
+        }),
+        activityId: string({
+            required_error: 'Activity ID is required',
+            invalid_type_error: 'Activity ID should be of type string'
+        })
+    }).strict('Request URL contains an invalid key')
+});
+
 const getEmployeeList = getBusinessById;
-// Same for now, hence borrowing
 
 const getActivityList = getBusinessById;
-// Same for now, hence borrowing
 
 const createBusiness = object({
     body: object({
@@ -68,31 +92,9 @@ const addNewActivity = object({
     })
 });
 
-const removeEmployee = object({
-    params: object({
-        businessId: string({
-            required_error: 'Business ID is required',
-            invalid_type_error: 'Business ID should be of type string'
-        }),
-        employeeId: string({
-            required_error: 'Employee Firebase ID is required',
-            invalid_type_error: 'Employee Firebase ID should be of type string'
-        })
-    }).strict('Request URL contains an invalid key')
-});
+const removeEmployee = getEmployee;
 
-const removeActivity = object({
-    params: object({
-        businessId: string({
-            required_error: 'Business ID is required',
-            invalid_type_error: 'Business ID should be of type string'
-        }),
-        activityId: string({
-            required_error: 'Activity ID is required',
-            invalid_type_error: 'Activity ID should be of type string'
-        })
-    }).strict('Request URL contains an invalid key')
-});
+const removeActivity = getActivity;
 
 const updateBusiness = object({
     params: object({
@@ -111,4 +113,60 @@ const updateBusiness = object({
     })
 });
 
-export { getBusinessById, getEmployeeList, getActivityList, createBusiness, addNewEmployee, addNewActivity, removeEmployee, removeActivity, updateBusiness };
+const updateEmployee = object({
+    params: object({
+        businessId: string({
+            required_error: 'Business ID is required',
+            invalid_type_error: 'Business ID should be of type string'
+        }),
+        employeeId: string({
+            required_error: 'Employee Firebase ID is required',
+            invalid_type_error: 'Employee Firebase ID should be of type string'
+        })
+    }).strict('Request URL contains an invalid key'),
+    body: object({
+        // Optional
+        firstName: string({ invalid_type_error: 'First Name should be of type string' }).optional(),
+        lastName: string({ invalid_type_error: 'Last Name should be of type string' }).optional(),
+        role: string({ invalid_type_error: 'Role should be of type string' }).optional()
+    })
+});
+
+const updateActivity = object({
+    params: object({
+        businessId: string({
+            required_error: 'Business ID is required',
+            invalid_type_error: 'Business ID should be of type string'
+        }),
+        activityId: string({
+            required_error: 'Activity ID is required',
+            invalid_type_error: 'Activity ID should be of type string'
+        })
+    }).strict('Request URL contains an invalid key'),
+    body: object({
+        // Optional
+        title: string({ invalid_type_error: 'Title should be of type string' }).optional(),
+        description: string({ invalid_type_error: 'Description should be of type string' }).optional(),
+        startTime: any().optional(),
+        endTime: any().optional(),
+        costPerIndividual: number({ invalid_type_error: 'Cost Per Individual should be of type number' }).optional(),
+        costPerGroup: number({ invalid_type_error: 'Cost Per Group should be of type number' }).optional(),
+        groupSize: number({ invalid_type_error: 'Group Size should be of type number' }).optional()
+    })
+});
+
+export {
+    getBusinessById,
+    getEmployee,
+    getActivity,
+    getEmployeeList,
+    getActivityList,
+    createBusiness,
+    addNewEmployee,
+    addNewActivity,
+    removeEmployee,
+    removeActivity,
+    updateEmployee,
+    updateActivity,
+    updateBusiness
+};
