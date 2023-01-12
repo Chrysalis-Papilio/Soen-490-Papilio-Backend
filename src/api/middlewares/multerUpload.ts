7ecf4089c6ed8110c060833bd42438d2d2530b2a
Thanh Ta <mth.1012.ta@gmail.com>
2023-01-11 22:54:01 -0500

    Added filter and limits for multer

--- src/api/middlewares/multerUpload.ts
+++ src/api/middlewares/multerUpload.ts
@@ -1,5 +1,25 @@
 import multer from 'multer';
+import { Request } from 'express';
+import { APIError } from '../../errors/api-error';
+import { httpStatusCode } from '../../types/httpStatusCodes';
 
 const storage = multer.memoryStorage();
 
-export const upload = multer({ storage: storage });
+const fileFilter = (_: Request, file: Express.Multer.File, callback: multer.FileFilterCallback) => {
+    if (file.mimetype.split('/')[0] === 'image') {
+        callback(null, true);
+    } else {
+        callback(new APIError('Invalid file type, must be image', 'Multer Upload', httpStatusCode.BAD_REQUEST));
+    }
+};
+
+const limits = {
+    files: 5, // 5 files max
+    fileSize: 1048576 // 10 megabytes max
+};
+
+export const upload = multer({
+    storage,
+    fileFilter,
+    limits
+});
