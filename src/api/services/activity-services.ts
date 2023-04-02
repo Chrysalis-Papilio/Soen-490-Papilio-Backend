77c4254edcf3a6c781153a5af7eac83b59b87f03
Thanh Ta <mth.1012.ta@gmail.com>
2023-04-02 13:52:07 -0400

    Matched all responses including Activity

--- src/api/services/activity-services.ts
+++ src/api/services/activity-services.ts
@@ -4,8 +4,8 @@ const getAllActivities = async (page: number, size: number) => {
     return activityRepo.getAllActivities(page, size);
 };
 
-const getActivity = async (id: number, contact: boolean = false) => {
-    return activityRepo.getActivity(id, contact);
+const getActivity = async (id: number) => {
+    return activityRepo.getActivity(id);
 };
 
 const updateActivity = async (id: number, update: any) => {
